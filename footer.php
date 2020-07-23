4e9ee5520629159029f1a1407ad3fbc6251f5e63
Joe Bailey-Roberts <dev@joebr.io>
2020-07-23 16:13:33 +0100

    Edit footer content

--- footer.php
+++ footer.php
@@ -4,15 +4,30 @@
 <footer class="site-footer">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-6"><p>&copy; Joe Bailey-Roberts <?php echo date('Y') ?></p></div>
-            <div class="col-12 col-sm-6"> <?php
+           
+            <div class="col-12 col-sm-6 col-lg-3"> <?php
 					wp_nav_menu(
 						array(
 							'theme_location' => 'footer',
 							'menu_id'        => 'footer-menu',
 						)
 					);
-					?></div>
+					?>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-3"> <?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'menu_id'        => 'footer-menu',
+						)
+					);
+					?>
+			</div>
+
+			<div class="col-12 col-lg-6 right">
+				<p>&copy; Joe Bailey-Roberts <?php echo date('Y') ?></p>
+			</div>
+
         </div>
         
 
