7dd030c64ab27aa10f946929512342c2a26cf549
Joe Bailey-Roberts <dev@joebr.io>
2020-07-28 11:10:15 +0100

    Add header logo

--- inc/utilities/filters.php
+++ inc/utilities/filters.php
@@ -15,7 +15,7 @@ function custom_logo() {
 	$html = sprintf(
 		'<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url">%2$s</a>',
 		esc_url( home_url( '/' ) ),
-		'<img src="' . assets\get_asset_path( 'logo.svg' ) . '" class="custom-logo"width="133" height="75">'
+		'<img src="' . assets\get_asset_path( 'logo.svg' ) . '" class="custom-logo"width="400" height="110">'
 	);
 	return $html;
 }
