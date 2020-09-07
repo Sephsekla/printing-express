424b6ec7de912571e0c183d3b4d67c3fe4121a9e
Joe Bailey-Roberts <dev@joebr.io>
2020-09-07 15:43:55 +0100

    Remove analytics

--- inc/setup.php
+++ inc/setup.php
@@ -92,34 +92,3 @@ add_theme_support( 'disable-custom-colors' );
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\init' );
 
-
-/**
- * Add Google Tag Manager
- */
-add_action('wp_head',function(){
-	?>
-
-	<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NBN8G4Z');</script>
-<!-- End Google Tag Manager -->
-
-<?php
-});
-
-/**
- * Add Noscript Google Tag Manager
- */
-add_action('wp_body_open',function(){
-	?>
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBN8G4Z"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
-<?php
-});
