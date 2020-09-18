81c4ac868a71a16d2800f5795dcd6b9dd1142a4f
Joe Bailey-Roberts <dev@joebr.io>
2020-09-18 14:19:10 +0100

    Add related product filters

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -29,3 +29,37 @@ function exclude_large_format( $q ) {
 
 }
 add_action( 'woocommerce_product_query', __NAMESPACE__.'\\exclude_large_format' );  
+
+
+// define the woocommerce_output_related_products_args callback 
+function filter_woocommerce_output_related_products_args( $args ) { 
+    
+    // make filter magic happen here... 
+
+    if(has_term('large-format','product_cat',null)){
+
+        $args['tax_query'][] = array(
+            'taxonomy' => 'product_cat',
+            'field' => 'slug',
+            'terms' => array( 'large-format' ), // Don't display products in the large format category on the shop page.
+            'operator' => 'IN'
+     );
+
+    }
+    else{
+
+        $args['tax_query'][] = array(
+            'taxonomy' => 'product_cat',
+            'field' => 'slug',
+            'terms' => array( 'large-format' ), // Don't display products in the large format category on the shop page.
+            'operator' => 'NOT IN'
+     );
+
+    }
+
+
+    return $args; 
+}; 
+         
+// add the filter 
+add_filter( 'woocommerce_output_related_products_args',  __NAMESPACE__.'\\filter_woocommerce_output_related_products_args', 10, 1);
