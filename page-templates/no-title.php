f329c527eee9310b0176a2b6ee51726417508318
Joe Bailey-Roberts <dev@joebr.io>
2020-09-21 11:52:18 +0100

    Add no title template

---
+++ page-templates/no-title.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template name: No Title
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package printing-express
+ */
+
+get_header();
+
+global $wp_query; 
+$found_posts = $wp_query->found_posts;
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+
+
+		<?php
+
+		get_template_part( 'template-parts/banner' );
+
+	
+		
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', 'notitle');
+			}
+		
+
+			the_posts_navigation();
+
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ template-parts/content-notitle.php
@@ -0,0 +1,12 @@
+<article <?php post_class(); ?>">
+	<div class="container">
+			 
+				<?php
+				the_content();
+				?>
+	   
+	  
+
+		
+	</div>
+</article>
