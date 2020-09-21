2d23f68d361c553ad8204c7b205d07f0c57cbe21
Joe Bailey-Roberts <dev@joebr.io>
2020-09-21 11:54:01 +0100

    Add homepage template

---
+++ page-templates/homepage.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template name: Homepage
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package printing-express
+ */
+
+get_header();
+
+global $wp_query; 
+$found_posts = $wp_query->found_posts;
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+	Banner deals go here
+
+		<?php
+
+		get_template_part( 'template-parts/banner' );
+		
+
+	
+		
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', 'notitle');
+			}
+		
+
+			the_posts_navigation();
+
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
