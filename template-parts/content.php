7b68954aa6e340c3482f709c8f944e028a6be41f
Joe Bailey-Roberts <dev@joebr.io>
2020-10-02 10:53:41 +0100

    Add breadcrumbs

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,15 +1,20 @@
 <article <?php post_class(); ?>">
-	<div class="container">
-	
-			  <h1><?php the_title(); ?></h1>
-			  <!-- <p class="date"><?php echo get_the_date( 'jS F Y' ); ?></p> -->
-			 
-				<?php
-				the_content();
-				?>
-	   
-	  
+    <div class="container">
+    <?php
+    if (function_exists('yoast_breadcrumb') ) {
+        yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+    }
+    ?>
+    
+              <h1><?php the_title(); ?></h1>
+              <!-- <p class="date"><?php echo get_the_date('jS F Y'); ?></p> -->
+             
+                <?php
+                the_content();
+                ?>
+       
+      
 
-		
-	</div>
+        
+    </div>
 </article>
