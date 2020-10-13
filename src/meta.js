5bdda85e71230f8f392016e3d7b16a1e8b007525
Joe Bailey-Roberts <dev@joebr.io>
2020-10-13 15:31:12 +0100

    Add colour picker

--- src/meta.js
+++ src/meta.js
@@ -4,15 +4,30 @@ const { PluginSidebar, PluginSidebarMoreMenuItem, PluginDocumentSettingPanel } =
 const { registerPlugin } = wp.plugins;
 const {TextControl, SelectControl} = wp.components;
 const {widthState} = wp.compose;
+const { withColors, PanelColorSettings, getColorClassName} = wp.blockEditor;
 
 const MetaBlockField = () => (
-    <SelectControl label="Banner Border" value="blue"
+    [<SelectControl label="Banner Border" value="blue"
     onChange={(value)=> {console.log(value)}} 
     options = {[
         {label: 'Blue', value: 'blue'},
         {label: 'Yellow', value: 'yellow'},
     ]}
-    />
+    />,
+    <PanelColorSettings
+          title={"Color Settings"}
+          colorSettings={[
+            {
+               // colors: colorSamples,
+              value: "blue",
+              onChange: (value)=> {console.log(value)},
+              label: "Banner Color"
+            }
+          ]}
+        />
+
+]
+    
 );
  
 const Component = () => (
