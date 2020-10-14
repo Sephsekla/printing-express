307035e01ee3dc0ad140c36590274f3216ea3e80
Joe Bailey-Roberts <dev@joebr.io>
2020-10-14 09:38:36 +0100

    Add social links

--- inc/utilities/assets.php
+++ inc/utilities/assets.php
@@ -39,16 +39,16 @@ else{
 
 <ul class="social">
     <li>
-        <a href="https://facebook.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/facebook.svg') ?>" width="28" height="28"></a>
+        <a href="https://www.facebook.com/Printing-Express-106452437808448/" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/facebook.svg') ?>" width="28" height="28"></a>
     </li>
     <li>
-        <a href="https://facebook.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/linkedin.svg') ?>" width="28" height="28"></a>
+        <a href="https://linkedin.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/linkedin.svg') ?>" width="28" height="28"></a>
     </li>
     <li>
-        <a href="https://facebook.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/instagram.svg') ?>" width="28" height="28"></a>
+        <a href="https://www.instagram.com/the_printing_express/" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/instagram.svg') ?>" width="28" height="28"></a>
     </li>
     <li>
-        <a href="https://facebook.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/twitter.svg') ?>" width="28" height="28"></a>
+        <a href="https://twitter.com" target="_blank"><img src="<?php echo get_asset_path('icons'.$folder.'/twitter.svg') ?>" width="28" height="28"></a>
     </li>
 </ul>
 
