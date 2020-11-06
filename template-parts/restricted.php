b4c9db95392a6cce739e7503e8bf7c2fc47460d3
Joe Bailey-Roberts <dev@joebr.io>
2020-11-06 16:08:17 +0000

    Tweak button

--- template-parts/restricted.php
+++ template-parts/restricted.php
@@ -3,7 +3,7 @@
   <h1>Restricted</h1>
   <p>This area is restricted to logged in users. Please login or sign up.</p>
 
-  <p><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" class="button">Login or Sign Up</a></p>
+  <p><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" class="btn">Login or Sign Up</a></p>
       
 
         
