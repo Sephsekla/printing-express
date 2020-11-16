9bceb60bec424d04c400074aaae5f0d35472abf7
Joe Bailey-Roberts <dev@joebr.io>
2020-11-16 14:28:49 +0000

    Add register link

--- template-parts/restricted.php
+++ template-parts/restricted.php
@@ -3,7 +3,18 @@
   <h1>Restricted</h1>
   <p>This area is restricted to logged in users. Please login or sign up.</p>
 
-  <?php woocommerce_login_form() ?>
+  <div class="woocommerce">
+  <h2>Login</h2>
+  <?php woocommerce_login_form(
+    [
+      'redirect' => get_permalink( 0 );
+    ]
+  ) ?>
+
+<p><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" class="btn">Register</a></p>
+
+
+  </div>
 
         
     </div>
