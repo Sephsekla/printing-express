0a05b49fea4120b28671a5e6db9c8118e2022512
Joe Bailey-Roberts <dev@joebr.io>
2020-11-17 14:04:17 +0000

    Get order by ID

--- inc/woocommerce/filters.php
+++ inc/woocommerce/filters.php
@@ -332,9 +332,7 @@ function my_custom_checkout_field_update_order_meta( $order_id ) {
 
 function output_large_format_details($order){
 
-    echo get_post_meta($order, 'Large Format Items');
-    print_r($order);
-    echo "TEST";
+    echo get_post_meta($order->get_id(), 'Large Format Items');
 
 }
 
