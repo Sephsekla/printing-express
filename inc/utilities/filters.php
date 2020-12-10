b0ccedc6c811e8d9a0ed67d9a67cf097bd88a3a4
Joe Bailey-Roberts <dev@joebr.io>
2020-12-10 12:24:01 +0000

    Add folders

--- inc/utilities/filters.php
+++ inc/utilities/filters.php
@@ -79,7 +79,7 @@ add_shortcode('custom_form_upload',function(){
 
     $folder = set_upload_folder(null);
 
-    return do_shortcode('[shareonedrive class="gf_upload_box" dir="'.$upload_folder.'" account="3d07ff6ba4270e23" mode="files" viewrole="administrator|author|contributor|editor|subscriber|guest" userfolders="auto" downloadrole="all" upload="1" uploadrole="all" delete="1" deletefilesrole="administrator|editor|customer|subscriber|shop_manager"]');
+    return do_shortcode('[shareonedrive class="gf_upload_box" dir="'.$upload_folder.'" account="3d07ff6ba4270e23" mode="files" viewrole="administrator|author|contributor|editor|subscriber|guest" userfolders="auto" downloadrole="all" upload="1" uploadrole="all" delete="1" deletefilesrole="administrator|editor|customer|subscriber|shop_manager" addfolder="1" addfolderrole="administrator|editor|customer|subscriber|shop_manager"]' );
 });
 
 /**
